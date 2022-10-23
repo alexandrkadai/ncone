cf0ff40451ff13d4fd3a7605acd4adb04bb86572
alex kadai <alexandrkadai@gmail.com>
2022-10-24 00:00:47 +0300

    Test Started

---
+++ src/components/favorites/favorites.component.jsx
@@ -0,0 +1,11 @@
+import './favorites.style.scss';
+
+const Favorites = () => {
+  return (
+    <div className="fav-container">
+      <span>favorites</span>
+    </div>
+  );
+};
+
+export default Favorites;
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,11 @@
+import './header.styles.scss';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h2>product list page</h2>
+    </div>
+  );
+};
+
+export default Header;
